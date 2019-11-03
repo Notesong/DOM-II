800e7e4f2b1b3cab29c954d18f32d03379624b5f
Notesong <notesong.git@outlook.com>
2019-11-03 09:38:07 -0500

    Send clicked blocks to top of stack

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -8,14 +8,19 @@ class Block {
     mouseUp() { this.position = 0; }
     // adjust position based on speed
     mouseDown() { this.position += this.speed; }
-    // jump block to top of stack
-    goToTop() {}
+    // send block to top of stack
+    goToTop(blockSel) {
+        blocks.prepend(blockSel); 
+    }
     // update the current position of the block on the x-axis
     // based on whether block was clicked and whether mouse is up or down
     // parameters:  block selector variable and mouse down flag
     updateStatus(blockSel, mousedown) {
         // check if mouse is up or down
-        if (mousedown === true) this.mouseDown();
+        if (mousedown === true) {
+            this.mouseDown();
+            this.goToTop(blockSel);
+        }
         else if (mousedown === false) this.mouseUp();
         // update block position accordingly on the x-axis
         blockSel.style.left = this.position + 'px';
@@ -32,12 +37,15 @@ const blueBlockSel = document.querySelector('.block--blue');
 const greenBlockSel = document.querySelector('.block--green');
 const pinkBlockSel = document.querySelector('.block--pink');
 const grayBlockSel = document.querySelector('.block--gray');
+// blocks selector for all blocks
+let blocks = document.querySelector('.blocks');
 
 // mouse down and up - sends blocks traveling to right of screen
 // mouse down flags
 let rmouse = false, bmouse = false, gremouse = false, pmouse = false, gramouse = false;
 // mouse down listeners for flags
 // when mouse is down, set appropriate flag to true
+// send block to top of stack
 redBlockSel.addEventListener('mousedown', () => { rmouse = true; });
 blueBlockSel.addEventListener('mousedown', () => { bmouse = true; });
 greenBlockSel.addEventListener('mousedown', () => { gremouse = true; });
@@ -52,14 +60,6 @@ window.addEventListener('mouseup', () => {
     pmouse = false;
     gramouse = false;
 });
-
-// listeners for clicks
-// sends appropriate block to top of stack
-redBlockSel.addEventListener('click', () => { redBlock.goToTop(); });
-blueBlockSel.addEventListener('click', () => { blueBlock.goToTop(); });
-greenBlockSel.addEventListener('click', () => { greenBlock.goToTop(); });
-pinkBlockSel.addEventListener('click', () => { pinkBlock.goToTop(); });
-grayBlockSel.addEventListener('click', () => { grayBlock.goToTop(); });
 
 // loop for game
 // updates the position of each block each game loop
